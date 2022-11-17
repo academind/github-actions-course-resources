a444452dfd49d19faeb3e24930b08de6e2395e29
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2022-11-17 18:37:19 +0100

    fixed docker action python deployment code

--- Code/08 Custom Actions/03 Finished Project/.github/actions/deploy-s3-docker/deployment.py
+++ Code/08 Custom Actions/03 Finished Project/.github/actions/deploy-s3-docker/deployment.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+import mimetypes
 from botocore.config import Config
 
 
@@ -14,9 +15,15 @@ def run():
 
     for root, subdirs, files in os.walk(dist_folder):
         for file in files:
-            s3_client.upload_file(os.path.join(root, file), bucket, file)
+            s3_client.upload_file(
+                os.path.join(root, file),
+                bucket,
+                os.path.join(root, file).replace(dist_folder + '/', ''),
+                ExtraArgs={"ContentType": mimetypes.guess_type(file)[0]}
+            )
 
     website_url = f'http://{bucket}.s3-website-{bucket_region}.amazonaws.com'
+    # The below code sets the 'website-url' output (the old ::set-output syntax isn't supported anymore - that's the only thing that changed though)
     with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
         print(f'website-url={website_url}', file=gh_output)
 
